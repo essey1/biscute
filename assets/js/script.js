8b87ac9bf37d81040ba473fcf0ca6bcc1d7d5647
Essey Tesfamichael Araya <111381905+essey1@users.noreply.github.com>
2023-06-15 19:56:17 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+/*==================== EVENTS SWIPER  ====================*/
+let swiperEvent = new Swiper(".event__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
