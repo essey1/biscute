e14eded0f2adb2042f7d556c3a5e8301646f38ca
essey1 <esseyt3@gmail.com>
2023-07-25 21:11:51 +0300

    minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,14 +100,14 @@ themeButton.addEventListener('click', () => {
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2500,
-    delay: 400,
+    duration: 1500,
+    delay: 250,
     // reset: true
 })
 
 sr.reveal(`.home__data`)
-sr.reveal(`.home__img`, {delay: 500})
-sr.reveal(`.home__social`, {delay: 600})
+sr.reveal(`.home__img`, {delay: 150})
+sr.reveal(`.home__social`, {delay: 250})
 sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
 sr.reveal(`.about__data, .contact__form`,{origin: 'right'})
 sr.reveal(`.steps__card, .product__card, .questions__group, .footer`,{interval: 100})
